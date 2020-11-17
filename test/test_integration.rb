4d83402a14d141b6bd72045af9d408060109d35c
Nate Berkopec <nate.berkopec@gmail.com>
2020-11-17 10:10:37 -0700

    Update

--- test/test_integration.rb
+++ test/test_integration.rb
@@ -4,15 +4,12 @@ class TestRPWIntegration < Minitest::Test
   LICENSE_KEY = "this-is-a-key"
   ADMIN_KEY = "this-is-a-admin-key"
 
-  def setup
-  end
-
   def test_setup
     matcher = nil
-    Open3.popen3("exe/rpw start") { |stdin, stdout, stderr|
+    Open3.popen3("exe/rpw start") do |stdin, stdout, stderr|
       stdin.close
       matcher = stdout.read
-    }
+    end
     assert_match "Welcome to the Rails Performance Workshop", matcher
   end
 end
